78856e9f6bdcdfce5c1e344df3e42a12505e3184
BW4YNE <62270458+BW4YNE@users.noreply.github.com>
2023-03-12 18:12:08 -0500

    Update PriceChecker_2.php

--- api/PriceChecker_2.php
+++ api/PriceChecker_2.php
@@ -175,16 +175,15 @@ if (!$db)
 }
 
 $result_1 = $db->query("use blah");
-$result_2 = $db->query("select * from ".$Search_Term2."");
+$result_2 = $db->query("select * from ".$Search_Term2.""); #<---Throws an error here and doesn't proceed with the code after
+$row_ct_2 = $result_2->num_rows; 
+	
 if (!$result_2)
 {
 	echo "This redirection is not working";
 	#readfile('https://csci440pricechecker.vercel.app/errorpage/pagenotfound.html');
     	exit;
-}
-$row_ct_2 = $result_2->num_rows; 
-	
-	
+}	
 
 
 if ($row_ct_2 > 0)
